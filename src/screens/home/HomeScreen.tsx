6f48262efa6a27117c4167eea3248259d2283147
raffinauvall <raffinauvaltaqy@gmail.com>
2026-02-08 13:06:00 +0700

    refactor: change header into component

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -3,6 +3,7 @@ import { Bell, Motorbike, Droplet, Zap } from "lucide-react-native";
 import CircularWidget from "@/components/home/CircularStats";
 import MotorCard from "@/components/home/MotorCards";
 import { useState } from "react";
+import Header from "@/components/home/Header";
 export default function HomeScreen() {
     const [motor, setMotor] = useState("Yamaha Scorpio")
     const [stats, setStats] = useState({
@@ -18,21 +19,10 @@ export default function HomeScreen() {
             contentContainerStyle={{ flexGrow: 1, padding: 24, backgroundColor: "#131313" }}
             showsVerticalScrollIndicator={false}
         >
-            {/* Header */}
-            <View className="flex-row items-center justify-between mb-8 pt-3">
-                <View className="">
-                    <Text className="text-gray-500 mb-1">Welcome Back</Text>
-                    <Text className="text-white text-2xl font-maisonBold" adjustsFontSizeToFit>Raffi Nauval</Text>
-                </View>
-                <View>
-                    <TouchableOpacity className="p-3 border border-gray-500 rounded-[10px]">
-                        <Bell size={20} color="#ffffff" />
-                    </TouchableOpacity>
-                </View>
-            </View>
+            <Header />
 
             {/* Stats Cards */}
-            <View className="space-y-4 mb-12">
+            <View className="space-y-4 mb-5">
                 <MotorCard motor={motor} onChangeMotor={changeMotor} />
 
                 <View className="flex-row gap-4 ">
@@ -56,7 +46,7 @@ export default function HomeScreen() {
             </View>
 
             {/* Start Tracking Button */}
-            <TouchableOpacity className="bg-[#34D399] py-5 rounded-3xl shadow-2xl mb-12">
+            <TouchableOpacity className="bg-[#34D399] py-5 rounded-3xl shadow-2xl mb-5">
                 <Text className="text-center font-maisonBold text-black text-lg">
                     Start Tracking
                 </Text>
@@ -64,7 +54,7 @@ export default function HomeScreen() {
 
             {/* Footer / Note */}
             <Text className="text-neutral-500 font-maison text-center">
-                Pastikan motor dalam kondisi menyala untuk tracking jarak tempuh.
+                Klik Start untuk mulai tracking motor anda
             </Text>
         </ScrollView>
     );
