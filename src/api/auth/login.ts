055e93042f96827656c3a89bfb2c72a00617c25e
raffinauvall <raffinauvaltaqy@gmail.com>
2026-02-09 00:48:11 +0700

    feat: add login function

---
+++ src/api/auth/login.ts
@@ -0,0 +1,7 @@
+import { supabase } from "../supabaseClient";
+
+export async function login(email: string, password: string) {
+    const { data, error } = await supabase.auth.signInWithPassword({ email, password })
+    if (error) throw error;
+    return data;
+}
