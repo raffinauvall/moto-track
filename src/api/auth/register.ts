f1dc9c7ecc2484722caf557402fc8461c72ab3cd
raffinauvall <raffinauvaltaqy@gmail.com>
2026-02-09 00:48:18 +0700

    feat: add register function

---
+++ src/api/auth/register.ts
@@ -0,0 +1,14 @@
+import { supabase } from "../supabaseClient";
+
+export async function register(name: string, email: string, password: string) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: { name }
+    }
+  });
+
+  if (error) throw error;
+  return data; 
+}
