3775ec66328425d5cec9a5dfdece9b18640290e4
raffinauvall <raffinauvaltaqy@gmail.com>
2026-02-10 03:26:37 +0700

    chore: redesign

--- src/screens/service/ServiceScreen.tsx
+++ src/screens/service/ServiceScreen.tsx
@@ -1,57 +1,38 @@
 import { ScrollView, View, Text, TouchableOpacity } from "react-native";
-import { Droplet, Wrench, Zap } from "lucide-react-native";
+import { ChevronRight, Droplet, Wrench } from "lucide-react-native";
 
 export default function ServiceScreen() {
-  const serviceParts = [
-    { name: "Oli Mesin", info: "500 km / 2000 km", icon: <Droplet size={20} color="#34D399" /> },
-    { name: "Busi", info: "1000 km / 5000 km", icon: <Zap size={20} color="#FBBF24" /> },
-    { name: "Filter Udara", info: "1500 km / 3000 km", icon: <Wrench size={20} color="#F87171" /> },
-    { name: "Rantai & Gir", info: "2000 km / 6000 km", icon: <Wrench size={20} color="#60A5FA" /> },
-    { name: "Rem Depan", info: "1200 km / 4000 km", icon: <Wrench size={20} color="#F97316" /> },
+  const serviceHistory = [
+    { type: "Service Ringan", date: "10 Feb 2026", icon: <Droplet size={30} color="#34D399" /> },
+    { type: "Service Berat", date: "15 Jan 2026", icon: <Wrench size={30} color="#FBBF24" /> },
+    { type: "Service Ringan", date: "20 Des 2025", icon: <Droplet size={30} color="#34D399" /> },
   ];
 
   return (
     <ScrollView className="flex-1 bg-[#131313] p-4 pt-12">
-      {/* Judul Halaman */}
       <Text className="text-white text-3xl font-bold mb-6">Service Motor</Text>
 
-      {/* Tombol Add Service History */}
-      <TouchableOpacity className="bg-green-500 p-4 rounded-xl mb-6 items-center shadow-lg">
+      <TouchableOpacity className="bg-green-500 p-4 rounded-xl mb-6 items-center shadow-md">
         <Text className="text-white font-bold text-lg">Add Service History</Text>
       </TouchableOpacity>
 
-      {/* Card Service Ringan */}
-      <View className="bg-[#212121] w-full p-4 rounded-xl shadow-md mb-6 pb-1">
-        {/* Header Card */}
-        <View className="flex-row justify-between items-center mb-4">
-          <Text className="text-2xl font-bold text-white">Service Ringan</Text>
-          <Text className="text-gray-400 text-sm">10 Feb 2026</Text>
-        </View>
-
-        {/* List Part */}
-        <View className="space-y-3 bg-gray-800 rounded-xl mb-5">
-          {serviceParts.map((part, index) => (
-            <View
-              key={index}
-              className="flex-row justify-between items-center  p-3 rounded-lg"
-            >
-              <View className="flex-row items-center space-x-3 ">
-                {part.icon}
-                <Text className="text-white font-medium ms-2">{part.name}</Text>
+      <View className="space-y-4">
+        {serviceHistory.map((service, index) => (
+          <TouchableOpacity
+            key={index}
+            className="bg-[#212121] rounded-xl p-4 flex-row justify-between items-center shadow-sm mb-3"
+            onPress={() => console.log(`Go to detail of ${service.type}`)}
+          >
+            <View className="flex-row items-center space-x-3 ">
+              {service.icon}
+              <View className="ms-3">
+                <Text className="text-white text-lg font-semibold">{service.type}</Text>
+                <Text className="text-gray-400 text-sm">{service.date}</Text>
               </View>
-              <Text className="text-gray-400 text-sm">{part.info}</Text>
             </View>
-          ))}
-        </View>
-        <View className="flex-row justify-between">
-                    <TouchableOpacity className="bg-green-500 w-[48%] p-4 rounded-xl mb-6 items-center shadow-lg">
-        <Text className="text-white font-bold text-lg">Edit </Text>
-      </TouchableOpacity>
-      <TouchableOpacity className="bg-red-500 p-4 rounded-xl w-[48%] mb-6 items-center shadow-lg">
-        <Text className="text-white font-bold text-lg">Delete</Text>
-      </TouchableOpacity>
-        </View>
-
+            <ChevronRight size={24} color="#ffffff" />
+          </TouchableOpacity>
+        ))}
       </View>
     </ScrollView>
   );
